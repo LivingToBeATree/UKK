511b146bd436abfb60d8d4cb1a07344874b241a7
LivingTree <aryarega2811@gmail.com>
2026-08-07 21:37:18 +0700

    change some of its initial schema and added the fillable inside

--- backend/comme-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/comme-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
+            $table->string('display_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('user');
+            $table->string('avatar')->nullable();
+            $table->text('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
