59985397c0f9e7e1f5be1d1219c70799dcd6e20e
LivingTree <aryarega2811@gmail.com>
2026-08-07 21:35:44 +0700

    added BelongTo and renamed following to follower because i feel like it

--- backend/comme-backend/app/Models/Follow.php
+++ backend/comme-backend/app/Models/Follow.php
@@ -3,21 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Follow extends Model
 {
     protected $fillable = [
+        'follower_id',
         'followed_id',
-        'following_id',
     ];
 
     // Relationships
-    public function follower()
+    public function follower(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'following_id');
+        return $this->belongsTo(User::class, 'follower_id');
     }
 
-    public function followed()
+    public function followed(): BelongsTo
     {
         return $this->belongsTo(User::class, 'followed_id');
     }
