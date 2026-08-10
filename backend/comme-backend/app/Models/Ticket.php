2ec351651631cb55d561afd4026797a1dc9aaaaf
LivingTree <aryarega2811@gmail.com>
2026-08-10 08:53:36 +0700

    added ticket message and adjust ticket's models

--- backend/comme-backend/app/Models/Ticket.php
+++ backend/comme-backend/app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Enum\TicketPriority;
 
 class Ticket extends Model
@@ -34,5 +35,15 @@ class Ticket extends Model
     public function assignee(): BelongsTo
     {
         return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function moderationActions(): HasMany
+    {
+        return $this->hasMany(ModerationAction::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(TicketMessage::class);
     }
 }
