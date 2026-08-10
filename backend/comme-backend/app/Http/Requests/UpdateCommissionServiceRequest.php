f67fa82cdaa0768ebc1f33bd91e7ae9e1d40f0f1
LivingTree <aryarega2811@gmail.com>
2026-08-10 16:45:04 +0700

    added requests for the commissionservicecontroller

---
+++ backend/comme-backend/app/Http/Requests/UpdateCommissionServiceRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+use App\Enum\ServiceStatus;
+
+
+class UpdateCommissionServiceRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('commission_service'));
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes','string','max:255'],
+            'description' => ['sometimes','string'],
+            'alt_text' => ['sometimes', 'nullable', 'string', 'max:255'],
+            'thumbnail_media_id' => ['sometimes', 'nullable', 'exists:medias,id'],
+            'status' => ['sometimes', new Enum(ServiceStatus::class)],
+        ];
+    }
+}
