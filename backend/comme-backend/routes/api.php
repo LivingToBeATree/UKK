7886aed0c84b0fdec4cc411c8dada9c0537ad959
LivingTree <aryarega2811@gmail.com>
2026-08-10 16:47:08 +0700

    added an api resource for the controller

--- backend/comme-backend/routes/api.php
+++ backend/comme-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArtistProfileController;
+use App\Http\Controllers\CommissionServiceController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class,'register']);
@@ -13,3 +14,4 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 Route::apiResource('artist-profiles', ArtistProfileController::class);
+Route::apiResource('commmission-services', CommissionServiceController::class);
