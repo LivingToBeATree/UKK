0527fe0398c52123dd8bdc95c04132d85aebf367
LivingTree <aryarega2811@gmail.com>
2026-08-10 12:18:40 +0700

    added HasApitokens from the sanctum install's info

--- backend/comme-backend/app/Models/User.php
+++ backend/comme-backend/app/Models/User.php
@@ -5,7 +5,8 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;                                                                                                                                                                                                                                                                                                                                  
+use Illuminate\Foundation\Auth\User as Authenticatable;  
+use Laravel\Sanctum\HasApiTokens;                                                                                                                                                                                                                                                                                                                                
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -14,7 +15,7 @@ use App\Enum\UserRole;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /** 
      * The attributes that are mass assignable.
