44c4ef1c169b93fb1c46aed3180fc2e65ea131ad
LivingTree <aryarega2811@gmail.com>
2026-08-11 13:20:57 +0700

    fixed a typo

--- backend/comme-backend/routes/API/V1/Portfolio.php
+++ backend/comme-backend/routes/API/V1/Portfolio.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\API\V1\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('commission-services', PortfolioController::class);
+Route::apiResource('portfolios', PortfolioController::class);
