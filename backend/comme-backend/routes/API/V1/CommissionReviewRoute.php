deee0b230c8e8a78a83d644bc36f3541d22f525e
LivingTree <aryarega2811@gmail.com>
2026-08-12 14:17:19 +0700

    removed unused routes

--- backend/comme-backend/routes/API/V1/CommissionReviewRoute.php
+++ backend/comme-backend/routes/API/V1/CommissionReviewRoute.php
@@ -7,7 +7,5 @@ Route::get('/artist-profiles/{artist_profile}/reviews', [CommissionReviewControl
 Route::post('/commissions/{commission}/reviews', [CommissionReviewController::class, 'store']);
 Route::get('/reviews/{review}', [CommissionReviewController::class, 'show']);
 Route::patch('/reviews/{review}', [CommissionReviewController::class, 'update']);
-Route::delete('/reviews/{review}', [CommissionReviewController::class,'destroy']);
-Route::patch('/reviews/{review}/reply', [CommissionReviewController::class,'reply']);
-
-Route::apiResource('commission-reviews', CommissionReviewController::class);
+Route::delete('/reviews/{review}', [CommissionReviewController::class, 'destroy']);
+Route::patch('/reviews/{review}/reply', [CommissionReviewController::class, 'reply']);
