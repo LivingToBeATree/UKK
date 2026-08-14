5e9f9ad1d11dc4c9ff8cb5435ff6dbd5781fa207
LivingTree <aryarega2811@gmail.com>
2026-08-14 13:00:55 +0700

    added MustVerifyEmail import

--- backend/comme-backend/app/Models/User.php
+++ backend/comme-backend/app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;  
-use Laravel\Sanctum\HasApiTokens;                                                                                                                                                                                                                                                                                                                                
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -17,7 +17,7 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
-    /** 
+    /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
