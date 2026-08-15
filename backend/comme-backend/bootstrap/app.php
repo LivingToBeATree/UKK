c869f9937b73fc704eb2e713c73f1c5a33a4230d
LivingTree <aryarega2811@gmail.com>
2026-08-15 20:57:33 +0700

    fixed a double semicolon bug

--- backend/comme-backend/bootstrap/app.php
+++ backend/comme-backend/bootstrap/app.php
@@ -33,7 +33,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (Throwable $e, Request $request) {
             if ($request->expectsJson() || $request->is('api/*')) {
-                return app(ApiExceptionHandler::class)->handle($e);;
+                return app(ApiExceptionHandler::class)->handle($e);
             }
         });
     })
