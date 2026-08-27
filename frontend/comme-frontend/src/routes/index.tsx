9f5e6a7e21193416e7904815793dba8a81ac9dbb
LivingTree <aryarega2811@gmail.com>
2026-08-27 12:46:09 +0700

    feat: refactor the PlaygroundPage

--- frontend/comme-frontend/src/routes/index.tsx
+++ frontend/comme-frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { PlaygroundPage } from '@/pages/PlaygroundPage';
+import { PlaygroundPage } from '@/pages/dev/PlaygroundPage';
 export { ProtectedRoute } from './protectedRoutes';
 export { GuestRoute } from './GuestRoute';
 
